7d494b3892f2456c4be207e7b311c9e7d767daf7
Moeen Ahmed <ahmed.moeen@visionetsystems.com>
2025-03-15 00:03:27 +0500

    init commit

---
+++ DotNetIdentityDemo/DotNetIdentityAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetIdentityAPI.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions):
+            base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
